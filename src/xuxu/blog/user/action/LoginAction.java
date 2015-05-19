450c23d5e39ef81509676bee2494dc838a75f611
shanjunpeng <523386104@qq.com>
2015-05-19 23:38:38 +0800

    1

---
+++ src/xuxu/blog/user/action/LoginAction.java
@@ -0,0 +1,25 @@
+package xuxu.blog.user.action;
+
+import xuxu.blog.common.action.BaseAction;
+import xuxu.blog.entity.User;
+
+public class LoginAction extends BaseAction {
+
+	private User user = new User();
+
+	public String execute() {
+		if ("monster".equalsIgnoreCase(user.getName())) {
+			return "fail";
+		}
+		return "success";
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
