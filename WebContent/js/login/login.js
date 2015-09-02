8fc5876e547931237fc05686c9e18f62f4d67f13
shanjunpeng <523386104@qq.com>
2015-09-02 16:02:30 +0800

    2015-9-2

--- WebContent/js/login/login.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$("#btn_register").click(function(){
-		location="register.jsp";
-	});
-	$("#img_verifyCode").click(function(){
-		document.getElementById('img_verifyCode').src='/xuxu/common/verifyCode?'+Math.random();
-	});
-});
